8d3b6009e9e6c9c0294415d2817f2616e23d4ebe
Deiiivy <deivypr28@gmail.com>
2024-03-27 14:49:21 -0500

    initializing project

---
+++ user-crud-asp.net/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace user_crud_asp.net.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
